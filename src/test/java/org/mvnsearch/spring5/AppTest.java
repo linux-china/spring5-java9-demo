b46b34fb5b830ed4846ed825092ab3415a62db1a
linux_china <linux_china@hotmail.com>
2016-12-05 10:33:52 +0800

    add @TestPropertySource

--- src/test/java/org/mvnsearch/spring5/AppTest.java
+++ src/test/java/org/mvnsearch/spring5/AppTest.java
@@ -3,7 +3,9 @@ package org.mvnsearch.spring5;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,13 +17,18 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
+@TestPropertySource(properties = {"timezone = GMT", "port: 4242"})
 public class AppTest {
 
     @Autowired
     private UserService userService;
 
+    @Value("${port}")
+    private Integer port;
+
     @Test
     public void testApp() {
+        System.out.println("port: " + port);
         assertThat(userService.findNickById(1L)).contains("nick");
     }
 }
