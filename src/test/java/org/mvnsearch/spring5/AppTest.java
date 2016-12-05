f33590adc901e546132729314aa0aa69a3ea6bab
linux_china <linux_china@hotmail.com>
2016-12-05 10:19:04 +0800

    simple spring app test

--- src/test/java/org/mvnsearch/spring5/AppTest.java
+++ src/test/java/org/mvnsearch/spring5/AppTest.java
@@ -1,18 +1,27 @@
 package org.mvnsearch.spring5;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * simple app test
+ * simple spring app test
  *
  * @author linux_china
  */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
 public class AppTest {
+
+    @Autowired
+    private UserService userService;
 
     @Test
     public void testApp() {
-        assertThat("good morning").contains("good");
+        assertThat(userService.findNickById(1L)).contains("nick");
     }
 }
