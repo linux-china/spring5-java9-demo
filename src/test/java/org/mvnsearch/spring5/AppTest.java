7524c557a83b12f00dcef35e49d96dd6b2619694
linux_china <linux_china@hotmail.com>
2017-06-21 17:28:41 +0800

    add @ValueSource

--- src/test/java/org/mvnsearch/spring5/AppTest.java
+++ src/test/java/org/mvnsearch/spring5/AppTest.java
@@ -1,6 +1,7 @@
 package org.mvnsearch.spring5;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,16 +19,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ContextConfiguration(classes = AppConfig.class)
 @TestPropertySource(properties = {"timezone = GMT", "port: 4242"})
 public class AppTest {
-
-    @Autowired
-    private UserService userService;
-
     @Value("${port}")
     private Integer port;
 
-    @Test
-    public void testApp() {
+    @ParameterizedTest
+    @ValueSource(strings = {"Hello", "World"})
+    public void testApp(String argument, @Autowired UserService userService) {
         System.out.println("port: " + port);
         assertThat(userService.findNickById(1L)).contains("nick");
+        System.out.println("argument: " + argument);
     }
+
 }
